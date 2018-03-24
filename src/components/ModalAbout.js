7c9178dd5e9a6f1365b04e56e1277b17e110121d
Fabiano Cunha <cunhasb@aol.com>
2018-03-24 02:47:25 -0400

    Create some messages and guessing game

--- src/components/ModalAbout.js
+++ src/components/ModalAbout.js
@@ -1,34 +1,106 @@
 import React, { Component } from "react";
-import { Button, Icon, Modal } from "semantic-ui-react";
+import {
+  Button,
+  Icon,
+  Modal,
+  Image,
+  List,
+  Transition
+} from "semantic-ui-react";
 import { connect } from "react-redux";
-
 class NestedModal extends Component {
-  state = { open: false };
+  state = { open: false, timer: "", messages: [], items: [] };
 
   open = () => this.setState({ open: true });
   close = () => this.setState({ open: false });
+  handleClick = e => {
+    let timer = setInterval(this.tick, 8000);
+    this.setState({
+      timer: timer,
+      items: [
+        `Let's begin by your gender appearance, I would say that I'm pretty confident your appear to be  ${
+          this.props.demographics.gender
+        }`
+      ]
+    });
+    console.log("clicked");
+  };
+  componentWillUnmout = () => {
+    clearInterval(this.state.timer);
+  };
+  tick = () => {
+    if (this.state.items.length < this.state.messages.length) {
+      this.setState({
+        items: this.state.messages.slice(0, this.state.items.length + 1)
+      });
+    } else {
+      clearInterval(this.state.timer);
+    }
+  };
 
+  componentDidMount = () => {
+    const messages = [
+      `Let's begin by your gender appearance, I would say that I'm pretty confident your appear to be  ${
+        this.props.demographics.gender
+      }`,
+      `Now, let me try to guess your age,don't worry I won't tell anyone, it's only between you and me. I would say that you are between ${
+        this.props.demographics.age[0]
+      } and ${this.props.demographics.age[1]}`,
+      `How Am I doing? Let's take it up a notch, I'm now going to try to guess your ethnicity. Be kind though, sometimes I get it wrong, you're most likely to be ${
+        this.props.demographics.ethnicity[0]
+      }, followed by ${this.props.demographics.ethnicity[1]} and ${
+        this.props.demographics.ethnicity[0]
+      }`,
+      `Your face is very familiar, you remind me of someone, not quite sure who, but your face is very familiar, are you a celebrity? Oh, yeah, you remind me of ${
+        this.props.celebrity[0]
+      }, or is it ${this.props.celebrity[1]}, well I even see some of ${
+        this.props.celebrity[2]
+      } in you.`
+    ];
+    this.setState(prevState => {
+      return { messages: messages };
+    });
+  };
   render() {
     console.log("props modal", this.props);
-    const { open } = this.state;
-    debugger;
+    console.log("state", this.state);
+    const { open, items } = this.state;
+    // debugger;
     return (
       <Modal
-        style={{ position: "relative", top: "50%", left: "50%" }}
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translateX(-50%)"
+        }}
         dimmer={false}
         open={open}
         onOpen={this.open}
         onClose={this.close}
         trigger={
-          <Button primary icon>
+          <Button primary icon onClick={this.handleClick}>
             Proceed <Icon name="right chevron" />
           </Button>
         }
         size="small"
       >
-        <Modal.Header>Modal #2</Modal.Header>
+        <Modal.Header>My Guesses</Modal.Header>
         <Modal.Content>
-          <p>{`That's everything!`}</p>
+          <Transition.Group
+            animation="scale"
+            as={List}
+            duration={400}
+            divided
+            size="huge"
+            verticalAlign="middle"
+          >
+            {items.map(item => (
+              <List.Item key={item}>
+                <List.Content header={item} />
+              </List.Item>
+            ))}
+          </Transition.Group>
         </Modal.Content>
         <Modal.Actions>
           <Button icon="check" content="All Done" onClick={this.close} />
@@ -49,13 +121,16 @@ const ModalExampleMultiple = props => (
     open={props.open}
     onClose={props.close}
   >
-    <Modal.Header>Modal #1</Modal.Header>
+    <Modal.Header>{`Let's Play a Game!`}</Modal.Header>
     <Modal.Content image>
       <div className="image">
         <Icon name="right arrow" />
       </div>
       <Modal.Description>
-        <p>We have more to share with you. Follow us along to modal 2</p>
+        <p>
+          {`I'm going to try to guess some things about you. Let me know if I'm
+          right`}
+        </p>
       </Modal.Description>
     </Modal.Content>
     <Modal.Actions>
